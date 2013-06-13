c1b13a97828ca3f76f6e5742a7f25551d4b33efe
Maksym Shevchenko <myshevchenko@luxoft.com>
2013-06-13 15:36:02 +0300

    Assign Copyright to LG Electronics, Inc.
    [GF-5644] Convert HP Copyright to LG
    
    Open-webOS-DCO-1.0-Signed-off-by: Maksym Shevchenko <myshevchenko@luxoft.com>
    
    Change-Id: I98fc9d3d29cdc736892105875b83f1e5580800ae
    Reviewed-on: https://g2g.palm.com/2592
    Reviewed-by: Build Verification
    Reviewed-by: Suresh Arumugam <suresh.arumugam@lge.com>
    Tested-by: Suresh Arumugam <suresh.arumugam@lge.com>

--- contacts/javascript/Utilities/Utils.js
+++ contacts/javascript/Utilities/Utils.js
@@ -1,6 +1,6 @@
 // LICENSE@@@
 //
-//      Copyright (c) 2009-2012 Hewlett-Packard Development Company, L.P.
+//      Copyright (c) 2009-2013 LG Electronics, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -16,7 +16,6 @@
 //
 // @@@LICENSE
 
-/* Copyright 2009 Palm, Inc.  All rights reserved. */
 /*jslint white: true, onevar: true, undef: true, eqeqeq: true, plusplus: true, bitwise: true,
 regexp: true, newcap: true, immed: true, nomen: false, maxerr: 500 */
 /*global _, exports, Assert, DisplayNameType, Person, Contact, RB, console, Future, Foundations,
